16780bb57f40ca0c1c11f6dfc098715b5a6f18cb
ccj <eatbye@gmail.com>
2020-05-14 21:20:12 +0800

    按钮点击的各种事件

--- components/common/common-list.vue
+++ components/common/common-list.vue
@@ -7,7 +7,9 @@
 					<image class="rounded-circle mr-2"
 					:src="item.userpic"
 					style="width: 65rpx;height: 65rpx;"
-					lazy-load></image>
+					lazy-load
+					@click="openSpace"
+					></image>
 					<!-- 昵称发布时间 -->
 					<view>
 						<view class="font" style="line-height: 1.5;">
@@ -28,24 +30,32 @@
 				</view>
 			</view>
 			<!-- 标题 -->
-			<view class="font-md my-1">{{item.title}}</view>
+			<view class="font-md my-1" @click="openDetail">{{item.title}}</view>
 			<!-- 图片 -->
 			<image v-if="item.titlepic" :src="item.titlepic" style="height: 350rpx;" class="rounded w-100"></image>
 			<!-- 图标按钮 -->
 			<view class="flex align-center">
-				<view class="flex align-center justify-center flex-1 animated faster" hover-class="jello text-main">
+				<view class="flex align-center justify-center flex-1 animated faster" 
+				hover-class="jello text-main"
+				@click="doSupport('support')">
 					<text class="iconfont icon-dianzan2 mr-2"></text>
 					<text>{{item.support.support_count}}</text>
 				</view>
-				<view class="flex align-center justify-center flex-1 animated faster" hover-class="jello text-main">
+				<view class="flex align-center justify-center flex-1 animated faster" 
+				hover-class="jello text-main"
+				@click="doSupport('unsupport')">
 					<text class="iconfont icon-cai mr-2"></text>
 					<text>{{item.support.unsupport_count}}</text>
 				</view>
-				<view class="flex align-center justify-center flex-1 animated faster" hover-class="jello text-main">
+				<view class="flex align-center justify-center flex-1 animated faster" 
+				hover-class="jello text-main"
+				@click="openDetail">
 					<text class="iconfont icon-pinglun2 mr-2"></text>
 					<text>{{item.comment_count}}</text>
 				</view>
-				<view class="flex align-center justify-center flex-1 animated faster" hover-class="jello text-main">
+				<view class="flex align-center justify-center flex-1 animated faster" 
+				hover-class="jello text-main"
+				@click="openDetail">
 					<text class="iconfont icon-fenxiang mr-2"></text>
 					<text>{{item.share_num}}</text>
 				</view>
@@ -58,6 +68,25 @@
 		props:{
 			item: Object,
 			index: Number
+		},
+		methods:{
+			// 打开个人空间
+			openSpace() {
+				console.log('打开个人空间');
+			},
+			// 关注
+			follow(){
+				// 通知父组件
+				this.$emit('follow',this.index)
+			},
+			// 进入详情页
+			openDetail(){
+				console.log('进入详情页');
+			},
+			// 顶踩操作
+			doSupport(type){
+				console.log(type);
+			}
 		}
 	}
 </script>
