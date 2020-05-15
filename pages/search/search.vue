8ec493b9ab0f89eb61027bf05c2ca4c608f073db
ccj <eatbye@gmail.com>
2020-05-15 09:28:05 +0800

    搜索完成

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,23 +1,116 @@
 <template>
 	<view>
-		<view class="py-2 font-md px-2">搜索历史</view>
-		<view class="flex flex-wrap">
-			<view class="border rounded font mx-2 my-1 px-2" 
-			v-for="(item,index) in list" :key="index"
-			hover-class="bg-light">{{item}}</view>
-		</view>
+		<template v-if="searchList.length === 0">
+			<view class="py-2 font-md px-2">搜索历史</view>
+			<view class="flex flex-wrap">
+				<view class="border rounded font mx-2 my-1 px-2" 
+				v-for="(item,index) in list" :key="index"
+				hover-class="bg-light">{{item}}</view>
+			</view>
+		</template>
+		<template v-else>
+			<!-- 数据列表 -->
+			<block v-for="(item,index) in searchList" :key="index">
+				<common-list :item="item" :index="index"></common-list>
+			</block>
+		</template>
 	</view>
 </template>
 
 <script>
+	// 测试数据
+		const demo = [{
+			username:"昵称",
+			userpic:"/static/default.jpg",
+			newstime:"2019-10-20 下午04:30",
+			isFollow:false,
+			title:"我是标题",
+			titlepic:"/static/demo/datapic/11.jpg",
+			support:{
+				type:"support", // 顶
+				support_count:1,
+				unsupport_count:2
+			},
+			comment_count:2,
+			share_num:2
+		},
+		{
+			username:"昵称",
+			userpic:"/static/default.jpg",
+			newstime:"2019-10-20 下午04:30",
+			isFollow:false,
+			title:"我是标题",
+			titlepic:"",
+			support:{
+				type:"unsupport", // 踩
+				support_count:1,
+				unsupport_count:2
+			},
+			comment_count:2,
+			share_num:2
+		},
+		{
+			username:"昵称",
+			userpic:"/static/default.jpg",
+			newstime:"2019-10-20 下午04:30",
+			isFollow:false,
+			title:"我是标题",
+			titlepic:"",
+			support:{
+				type:"", // 未操作
+				support_count:1,
+				unsupport_count:2
+			},
+			comment_count:2,
+			share_num:2
+		}];
+	
+	import commonList from '@/components/common/common-list.vue';
+		
 	export default {
+		components: {
+			commonList
+		},
 		data() {
 			return {
-				list:['uni-app第二季商城类实战开发','uni-app第三季仿微信实战开发','实战教程','系列教程']
+				searchText:"",
+				list:['uni-app第二季商城类实战开发','uni-app第三季仿微信实战开发','实战教程','系列教程'],
+				// 搜索结果
+				searchList:[]
+			}
+		},
+		// 监听导航输入
+		onNavigationBarSearchInputChanged(e){
+			this.searchText = e.text
+		},
+		// 监听点击导航搜索按钮
+		onNavigationBarButtonTap(e) {
+			if (e.index === 0) {
+				this.searchEvent()
 			}
 		},
 		methods: {
-			
+			// 点击搜索历史
+			clickSearchHistory(text){
+				this.searchText = text
+				this.searchEvent()
+			},
+			// 搜索事件
+			searchEvent(){
+				// 收起键盘
+				uni.hideKeyboard()
+				// 显示loading状态
+				uni.showLoading({
+					title: '加载中...',
+					mask: false
+				})
+				// 请求搜索
+				setTimeout(()=>{
+					this.searchList = demo
+					// 隐藏loading
+					uni.hideLoading()
+				},3000)
+			}
 		}
 	}
 </script>
